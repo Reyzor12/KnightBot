676c699780d98409f722aaec508fa96db3d7c358
Reyzor <testaccwow001@gmail.com>
2018-06-07 00:07:42 +0300

    play list not done

--- src/main/java/com/reyzor/discordbotknight/playlist/Playlist.java
+++ src/main/java/com/reyzor/discordbotknight/playlist/Playlist.java
@@ -3,14 +3,24 @@ package com.reyzor.discordbotknight.playlist;
 import java.util.List;
 
 /**
+ * Playlist for {@link com.reyzor.discordbotknight.bots.Bot}
  * @author reyzor
  * @version 1.0
  * @since 06.06.2018
  */
-public class Playlist {
+public class Playlist
+{
 
     private final String name;
     private final List<String> items;
-    private final List<PlaylistLoadError>
+    private final boolean shuffle;
+    //private List<PlaylistLoadError>
+
+    private Playlist(String name, List<String> items, boolean shuffle)
+    {
+        this.name = name;
+        this.items = items;
+        this.shuffle = shuffle;
+    }
 
 }
